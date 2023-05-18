1102926f48fdc7bf50058caf3919855591ca8e96
tengis-batsaikhan <126446746+tengis-batsaikhan@users.noreply.github.com>
2023-05-18 13:17:20 +0800

    Add files via upload

---
+++ new.js
@@ -0,0 +1,96 @@
+[
+  {
+    teamName: "1",
+    teamMembers: ["enkh-amar", "bulgankhuu", "oyun"],
+    phoneNum: 91208823,
+    email: "enkhamar@hobbyschool.edu.mn",
+  },
+
+  {
+    teamName: "2",
+    teamMembers: ["khulan", "mergedei", "yesudari"],
+    phoneNum: 85833048,
+    email: "khulangan-ochir@hobbyschool.edu.mn",
+  },
+
+  {
+    teamName: "Powerful Three",
+    teamMembers: ["Naidan", "Bilguun", "Erdenebat"],
+    phoneNum: 80080905,
+    email: "naidan.mb@gmail.com",
+  },
+
+  {
+    teamName: "4",
+    teamMembers: ["Ser Od", "Battushig", "Tegshjargal"],
+    phoneNum: 94919155,
+    email: "Ser-od@hobbyschool.edu.mn",
+  },
+
+  {
+    teamName: "5",
+    teamMembers: ["Genden", "Unukhishig", "Elbeg"],
+    phoneNum: 99190655,
+    email: "genden@hobbyschool.edu.mn",
+  },
+
+  {
+    teamName: "6",
+    teamMembers: ["Egshig", "Tolium", "Ujin"],
+    phoneNum: 99193568,
+    email: "egshig@hobbyschool.edu.mn",
+  },
+
+  {
+    teamName: "7",
+    teamMembers: ["enkh-amidral", "uchral", "enerelt"],
+    phoneNum: 90209020,
+    email: "enkh-amidral@hobbyschool.edu.mn",
+  },
+
+  {
+    teamName: "8",
+    teamMembers: [
+      "irmuunulziisaikhan",
+      "eneruunchinbat",
+      "tugskhuslenbatbayar",
+    ],
+    phoneNum: 94794884,
+    email: "irmuunulziisaikhan@hobbyschool.edu.mn",
+  },
+
+  {
+    teamName: "9",
+    teamMembers: ["amarlin", "", ""],
+    phoneNum: 0,
+    email: "amarlin@hobbyschool.edu.mn",
+  },
+
+  {
+    teamName: "10",
+    teamMembers: ["tugsuu.urg", "", ""],
+    phoneNum: 0,
+    email: "tugsuu.urg@gmail.com",
+  },
+
+  {
+    teamName: "Weateitup",
+    teamMembers: ["Sergelen", "Nomundari", "Munkhjin"],
+    phoneNum: 99861664,
+    email: "sergelen@hobbyschool.edu.mn",
+  },
+
+  {
+    teamName: "12",
+    teamMembers: ["Nomunsaikhan", "Amarlin", "Engun"],
+    phoneNum: 90600110,
+    email: "nomunsaikhanaltanshagagia@hobbyschool.edu.mn",
+  },
+
+  {
+    teamName: "13",
+    teamMembers: ["Nomin-Erdene", "Gerelt-Od", "Sanchirnomuun"],
+    phoneNum: 94155344,
+    email: "Nomin-ErdeneErdeneChuulgan@hobbyschool.edu.mn",
+  },
+];
